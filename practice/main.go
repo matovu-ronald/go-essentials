03a6d46eb4be0e73fba24a4f6bf8d3d966d05da7
Matovu Ronald <maronwhite6@gmail.com>
2022-09-11 13:56:18 +0300

    Get input from the console

--- practice/main.go
+++ practice/main.go
@@ -1,32 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-const aConst int = 64
-
 func main() {
-	var aString string = "This is pure Ug Go"
-	fmt.Println(aString)
-	fmt.Printf("The variable's type is %T\n", aString)
-
-	var anInteger int = 20
-	fmt.Println(anInteger)
-
-	var defaultInt int
-	fmt.Println(defaultInt)
-
-	var anotherString = "This is another string"
-	fmt.Println(anotherString)
-	fmt.Printf("The variable's type is %T\n", anotherString)
-
-	var anotherInt = 52
-	fmt.Println(anotherInt)
-	fmt.Printf("The variable's type is %T\n", anotherInt)
-
-	myString := "This is also a string"
-	fmt.Println(myString)
-	fmt.Printf("The vairable's type is %T\n", myString)
-
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	input, _ := reader.ReadString('\n')
+	fmt.Println("You entered: ", input)
 }
