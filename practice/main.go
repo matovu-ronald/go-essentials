b43c018af20c14a0e21c21878937d8a8cffd4e8e
Matovu Ronald <maronwhite6@gmail.com>
2022-09-12 16:51:06 +0300

    Using for loops

--- practice/main.go
+++ practice/main.go
@@ -1,28 +1,40 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
 func main() {
-	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+	colors := []string{"Red", "Green", "Blue"}
+	fmt.Println(colors)
 
-	var result string
-
-	switch dow {
-	case 1:
-		result = "It's Sunday!"
-		// fallthrough
-	case 2:
-		result = "It's Monday!"
-		// fallthrough
-	default:
-		result = "It's some other day!"
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
 	}
 
-	fmt.Println(result)
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	for _, color := range colors {
+		fmt.Println(color)
+	}
+
+	value := 1
+
+	for value < 10 {
+		fmt.Println("Value: ", value)
+		value++
+	}
+
+	sum := 1
+
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("Sum: ", sum)
+		if sum > 200 {
+			goto theEnd
+		}
+	}
+
+theEnd:
+	fmt.Println("End of the program")
 }
