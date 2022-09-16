b65aee39e4ed1f52aeb5a47cec7789f032d596a0
Matovu Ronald <maronwhite6@gmail.com>
2022-09-16 17:27:59 +0300

    Define functions as methods of custom types

--- practice/main.go
+++ practice/main.go
@@ -3,31 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	doSomething()
-	sum := addValues(3, 5)
-	fmt.Println("The sum is", sum)
-
-	multiSum, multiCount := addAllValues(4, 7, 9)
-	fmt.Println("Sum of mulitple values: ", multiSum)
-	fmt.Println("Count of items: ", multiCount)
+	poodle := Dog{"Poodle", 10, "Wuuuwuu"}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\nSound: %v\n", poodle.Breed, poodle.Weight, poodle.Sound)
+	poodle.Speak()
+	poodle.Sound = "Arf!"
+	poodle.Speak()
+	poodle.SpeakThreeTimes()
 }
 
-func doSomething() {
-	fmt.Println("Doing something")
+// Dog is a struct
+type Dog struct {
+	Breed  string
+	Weight int
+	Sound  string
 }
 
-// func addValues(value1 int, value2 int) int {
-// 	return value1 + value2
-// }
-
-func addValues(value1, value2 int) int {
-	return value1 + value2
+// Speak is how the dog barks
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
 }
 
-func addAllValues(values ...int) (int, int) {
-	total := 0
-	for _, v := range values {
-		total += v
-	}
-	return total, len(values)
+// SpeakThreeTimes is how the dog barks three times
+func (d Dog) SpeakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
